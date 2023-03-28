50ebaad7603e5f8d0192396da2e997f2128c59e6
Arunnetha1 <129146438+Arunnetha1@users.noreply.github.com>
2023-03-28 19:06:31 +0530

    Add files via upload

---
+++ eventbooking/eventbooking.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventbooking',
+  templateUrl: './eventbooking.component.html',
+  styleUrls: ['./eventbooking.component.css']
+})
+export class EventbookingComponent {
+
+}
